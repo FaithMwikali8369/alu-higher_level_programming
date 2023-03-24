c206372da153b12fc4520411b96595bf6ae6bb76
FaithMwikali8369 <f.mwikali@alustudent.com>
2023-03-24 04:28:48 -0700

    updated

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-case:my_list=[1, 2 , 3]
-case:my_list=[1]
+[1, 2, 3]
+[1, 2, 3]
--- python-everything_is_object/19-main.py
+++ python-everything_is_object/19-main.py
@@ -2,12 +2,19 @@
 copy_list = __import__('19-copy_list').copy_list
 
 my_list = [1, 2, 3]
-print(my_list)
+[1]
+[1]
+True
+False
 
 new_list = copy_list(my_list)
+[1, '2', 3]
+[1, '2', 3]
+True
+False
 
-print(my_list)
-print(new_list)
-
-print(new_list == my_list)
-print(new_list is my_list)
+print(new_list =(new_list)
+        []
+        []
+        True
+False
